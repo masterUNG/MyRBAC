2b0cecc539f507e5b112f87de9dcc62a8d59e8c7
MasterUNG <phrombutr@gmail.com>
2017-08-05 15:46:40 +0700

    finish 5Aug

--- app/src/main/java/app/ewtc/masterung/myrbac/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/myrbac/MainActivity.java
@@ -7,6 +7,7 @@ import app.ewtc.masterung.myrbac.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
