75320ca5370fbb5846b0d54aff91c16fef478420
MasterUNG <phrombutr@gmail.com>
2017-08-05 15:31:36 +0700

    Before Lunch

--- app/src/main/java/app/ewtc/masterung/myrbac/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/myrbac/MainActivity.java
@@ -3,11 +3,24 @@ package app.ewtc.masterung.myrbac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.ewtc.masterung.myrbac.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment
+        if (savedInstanceState == null) {
+            MainFragment mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mainContainer, mainFragment)
+                    .commit();
+        }
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/app/ewtc/masterung/myrbac/fragment/MainFragment.java
@@ -0,0 +1,26 @@
+package app.ewtc.masterung.myrbac.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.ewtc.masterung.myrbac.R;
+
+/**
+ * Created by masterung on 8/5/2017 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.main_fragment, container, false);
+        return view;
+
+    }
+}   // Main Class นี่คือ คลาสหลัก
