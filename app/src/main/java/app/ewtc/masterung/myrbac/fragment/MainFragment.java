964a780a4538380f402ea0f98d96ceb40fa559d3
MasterUNG <phrombutr@gmail.com>
2017-08-06 12:12:22 +0700

    Before Lunch

--- app/src/main/java/app/ewtc/masterung/myrbac/fragment/MainFragment.java
+++ app/src/main/java/app/ewtc/masterung/myrbac/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import app.ewtc.masterung.myrbac.R;
 
@@ -21,6 +22,35 @@ public class MainFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.main_fragment, container, false);
         return view;
+
+    }   // onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Register Controller
+        registerController();
+
+    }
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SignUpFragment signUpFragment = new SignUpFragment();
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.mainContainer, signUpFragment)
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+
 
     }
 }   // Main Class นี่คือ คลาสหลัก
