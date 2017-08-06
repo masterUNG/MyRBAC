db60ba851bd94119325fe608ed1005ea963755a6
MasterUNG <phrombutr@gmail.com>
2017-08-06 13:11:55 +0700

    After Lunch

--- app/src/main/java/app/ewtc/masterung/myrbac/fragment/SignUpFragment.java
+++ app/src/main/java/app/ewtc/masterung/myrbac/fragment/SignUpFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import app.ewtc.masterung.myrbac.R;
 
@@ -21,6 +22,16 @@ public class SignUpFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_signup, container, false);
         return view;
+
+    }   // onCreateView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Back Controller
+        ImageView imageView = getView().findViewById(R.id.imvBack);
+
 
     }
 }   // Main Class
